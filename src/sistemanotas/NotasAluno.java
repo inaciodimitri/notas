5aad6186a5f6fae205fbb24f946ed477a21b8a69
Usuario <Usuario@LEIA-PC>
2025-05-15 17:59:33 -0300

    classes e atributos - notas

---
+++ src/sistemanotas/NotasAluno.java
@@ -0,0 +1,56 @@
+
+package sistemanotas;
+
+public class NotasAluno {
+ 
+    private double nota1;
+    private double nota2;
+    private double nota3;
+
+
+    public NotasAluno(double nota1, double nota2, double nota3) {
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+   
+    public double calcularMedia() {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+
+    
+    public String situacao() {
+        double media = calcularMedia();
+        if (media >= 6.0) {
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+}
